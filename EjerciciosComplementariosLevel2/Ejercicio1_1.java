4795ec007374c6ca417597e930dd37124963c743
sebastyann12 <sebastyann1221@gmail.com>
2021-10-15 19:26:15 -0300

    1

--- EjerciciosComplementariosLevel2/Ejercicio1_1.java
+++
@@ -1,7 +0,0 @@
-public class Ejercicio1_1 {
-
-    public static void main(String[] args) {
-        System.out.println("hola mundo");
-        System.out.println("holo");
-    }
-}
---
+++ EjerciciosComplementariosLevel2/Ejercicio2_1.java
@@ -0,0 +1,15 @@
+
+
+public class Ejercicio2_1 {
+ 
+    public static void main(String[] args) {
+        
+        String [] ciudadesFav = {"#1 - Bariloche", "#2 - Buenos Aires", "#3 - Resistencia"};
+
+        for (int i = 0; i < ciudadesFav.length; i++) {
+            System.out.println(ciudadesFav[i]);
+        }
+    }
+    
+     
+}
